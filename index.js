4ec23dd7e1052964abed9fb11049a95ac93af17f
ViktorVan22 <zcg5368@163.com>
2022-07-06 09:38:22 +0800

    添加Node命令行工具commander

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi, I am Node.js");
+const program = require("commander");
+
+program
+  .option("-d, --debug", "output extra debugging")
+  .option("-s, --small", "small pizza size")
+  .option("-p, --pizza-type <type>", "flavour of pizza");
+program
+  .command("add")
+  .description("add a task")
+  .action((...args) => {
+    const words = args.slice(0, -1).join(" ");
+    console.log(words);
+  });
+program
+  .command("clear")
+  .description("clear all tasks")
+  .action((...args) => {
+    console.log("this is clear");
+  });
+
+program.parse(process.argv);
+
+// console.log(program.debug);

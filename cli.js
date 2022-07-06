f22904d34ff2f0063a610d910e0e6cb13f5e38ac
ViktorVan22 <zcg5368@163.com>
2022-07-06 10:28:02 +0800

    添加add命令

---
+++ cli.js
@@ -0,0 +1,24 @@
+const program = require("commander");
+const api = require("./index.js");
+
+program
+  .option("-d, --debug", "output extra debugging")
+  .option("-s, --small", "small pizza size")
+  .option("-p, --pizza-type <type>", "flavour of pizza");
+program
+  .command("add")
+  .description("add a task")
+  .action((...args) => {
+    // const words = args.slice(0, -1).join(" ");
+    api.add(args);
+  });
+program
+  .command("clear")
+  .description("clear all tasks")
+  .action((...args) => {
+    console.log("this is clear");
+  });
+
+program.parse(process.argv);
+
+// console.log(program.debug);
--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const program = require("commander");
+const homedir = require("os").homedir();
+const home = process.env.home || homedir;
 
-program
-  .option("-d, --debug", "output extra debugging")
-  .option("-s, --small", "small pizza size")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
-program
-  .command("add")
-  .description("add a task")
-  .action((...args) => {
-    const words = args.slice(0, -1).join(" ");
-    console.log(words);
+// 处理路径的
+const p = require("path");
+const dbPath = p.join(home, ".todo");
+
+const fs = require("fs");
+
+//导出的所有功能
+module.exports.add = title => {
+  console.log("title: ", title);
+  // 读取之前的任务
+  fs.readFile(dbPath, { flag: "a+" }, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let list;
+      try {
+        list = JSON.parse(data.toString());
+      } catch (error2) {
+        list = [];
+      }
+      const task = {
+        title: title,
+        done: false,
+      };
+      console.log("task: ", task);
+      list.push(task);
+      const string = JSON.stringify(list);
+      fs.writeFile(dbPath, string + "\n", error3 => {
+        if (error3) {
+          console.log(error3);
+        }
+      });
+    }
   });
-program
-  .command("clear")
-  .description("clear all tasks")
-  .action((...args) => {
-    console.log("this is clear");
-  });
-
-program.parse(process.argv);
-
-// console.log(program.debug);
+  // 往里面添加一个title任务
+  // 存储任务到文件
+};

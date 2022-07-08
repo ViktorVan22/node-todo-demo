86b1a1441a8750809a5df98abd5c83a346015cd2
ViktorVan22 <zcg5368@163.com>
2022-07-08 11:17:36 +0800

    配置debug文件

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require("inquirer");
 //导出的所有功能
 module.exports.add = async title => {
   // 读取之前的任务
+  debugger;
   const list = await db.read();
   // 往里面添加一个title任务
   list.push({ title, done: false });
